edfe78e6a2255d8f6defae78bc33b1af54d4ba88
Luferov1 <ldima12022002@gmail.com>
2025-08-28 15:09:27 +0300

    feat: add global storage connected to local storage. Minor refactoring of eslint rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
+import { AppProvider } from '@/src/context/AppContext';
+
 import './globals.css';
 import Header from '../components/Header';
 
@@ -13,8 +15,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
+        <AppProvider>
+          <Header />
+          <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
+        </AppProvider>
       </body>
     </html>
   );
